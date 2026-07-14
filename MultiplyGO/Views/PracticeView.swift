f47fb6c56033a98b342af785c865254f8381e948
nemoyworks <nemoyworks@gmail.com>
2026-07-14 18:16:41 +0300

    Implement table selection with Set in PracticeView

--- MultiplyGO/Views/PracticeView.swift
+++ MultiplyGO/Views/PracticeView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct PracticeView: View {
+    @State private var table = Int()
     @State private var selectedTable: Set<Int> = []
     @State private var startButton = false
     @State private var showAlert = false
@@ -22,19 +23,31 @@ struct PracticeView: View {
                 
                 HStack(spacing: 13) {
                     Button {
-                        
+                        if selectedTable.contains(1) {
+                            print(selectedTable)
+                        } else {
+                            selectedTable.insert(1)
+                        }
                     } label: {
                         TableCard(image: "TableOfOne")
                     }
                     
                     Button {
-                        
+                        if selectedTable.contains(2) {
+                            selectedTable.remove(2)
+                        } else {
+                            selectedTable.insert(2)
+                        }
                     } label: {
                         TableCard(image: "TableOfTwo")
                     }
                     
                     Button {
-                        
+                        if selectedTable.contains(3) {
+                            selectedTable.remove(3)
+                        } else {
+                            selectedTable.insert(3)
+                        }
                     } label: {
                         TableCard(image: "TableOfThree")
                     }
@@ -42,19 +55,31 @@ struct PracticeView: View {
                 
                 HStack(spacing: 13) {
                     Button {
-                        
+                        if selectedTable.contains(4) {
+                            selectedTable.remove(4)
+                        } else {
+                            selectedTable.insert(4)
+                        }
                     } label: {
                         TableCard(image: "TableOfFour")
                     }
                     
                     Button {
-                        
+                        if selectedTable.contains(5) {
+                            selectedTable.remove(5)
+                        } else {
+                            selectedTable.insert(5)
+                        }
                     } label: {
                         TableCard(image: "TableOfFive")
                     }
                     
                     Button {
-                        
+                        if selectedTable.contains(6) {
+                            selectedTable.remove(6)
+                        } else {
+                            selectedTable.insert(6)
+                        }
                     } label: {
                         TableCard(image: "TableOfSix")
                     }
@@ -62,19 +87,31 @@ struct PracticeView: View {
                 
                 HStack(spacing: 13) {
                     Button {
-                        
+                        if selectedTable.contains(7) {
+                            selectedTable.remove(7)
+                        } else {
+                            selectedTable.insert(7)
+                        }
                     } label: {
                         TableCard(image: "TableOfSeven")
                     }
                     
                     Button {
-                        
+                        if selectedTable.contains(8) {
+                            selectedTable.remove(8)
+                        } else {
+                            selectedTable.insert(8)
+                        }
                     } label: {
                         TableCard(image: "TableOfEight")
                     }
                     
                     Button {
-                        
+                        if selectedTable.contains(9) {
+                            selectedTable.remove(9)
+                        } else {
+                            selectedTable.insert(9)
+                        }
                     } label: {
                         TableCard(image: "TableOfNine")
                     }
@@ -105,8 +142,6 @@ struct PracticeView: View {
             
         }
     }
-    
-    
 }
 
 #Preview {
